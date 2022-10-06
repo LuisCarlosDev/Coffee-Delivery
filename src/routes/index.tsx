d7bc987b1196b5a59cee3b8724bcde0516d2525b
Luis Carlos Silva <luis.silva@smgbit.com.br>
2022-10-06 00:59:18 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from '../pages/Home'
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
